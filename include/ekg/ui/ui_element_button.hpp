757ff6d6fa7afdf4337bced03c01e6e40ae02c14
Rina <37088203+SirRina@users.noreply.github.com>
2022-09-21 18:47:44 -0300

    Removed setting visual UI.

---
+++ include/ekg/ui/ui_element_button.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef EKG_UI_ELEMENT_BUTTON_H
+#define EKG_UI_ELEMENT_BUTTON_H
+
+#include "ui_element.hpp"
+
+namespace ekg {
+	class button : ekg::element {
+	protected:
+		bool value {};
+	public:
+		void set_value(bool value);
+		bool get_value();
+	};
+}
+
+#endif
--- include/ekg/ui/value/ui_value_boolean.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-#ifndef EKG_UI_VALUE_BOOLEAN_H
-#define EKG_UI_VALUE_BOOLEAN_H
-
-#endif
