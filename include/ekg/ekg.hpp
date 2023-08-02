ea070e657f114fc5c05b4f64be375743f1fd08cf
Rina <37088203+MrsRina@users.noreply.github.com>
2023-08-02 17:58:40 -0300

    [devlog] version updates

--- include/ekg/ekg.hpp
+++ include/ekg/ekg.hpp
@@ -29,7 +29,7 @@
 #include "ekg/ui/textbox/ui_textbox.hpp"
 #include "ekg/ui/scroll/ui_scroll.hpp"
 
-#define EKG_VERSION_ID "0.0.1.0.2.0"
+#define EKG_VERSION_ID "0.0.1.0.3.0"
 #define EKG_VERSION_STATE "ALPHA"
 
 namespace ekg {
